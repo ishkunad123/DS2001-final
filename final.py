05584d6e8502bd8a0f86a92575b0fb77b1150be8
gonzalez.ali <alinajg24@gmail.comgit config --global user.email alinajg24@gmail.com>
2022-11-16 00:14:58 -0500

    adding

--- final.py
+++ final.py
@@ -1,2 +1,15 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
+
+VIS_ZERO_CRASH = "vis_zero_crash_rec.csv"
+
+def read_data(file): 
+    df = pd.read_csv(file)
+    return df
+    
+
+def main(): 
+    vis_crash = read_data(VIS_ZERO_CRASH)
+    print(vis_crash)
+main()
