c23dc7797e7593875f3069e1680facde2fe8d9f8
ishkunad123 <118331494+ishkunad123@users.noreply.github.com>
2022-11-21 16:28:31 -0500

    Update final.py

--- final.py
+++ final.py
@@ -8,7 +8,7 @@ def read_data(file):
     df = pd.read_csv(file)
     return df
     # aaaaaaaaaaaaaaaaaaaaaaa
-
+# ishani was here
 def main(): 
     vis_crash = read_data(VIS_ZERO_CRASH)
     print(vis_crash)
